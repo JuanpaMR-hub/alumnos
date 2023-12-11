60cf130408852f703f1c6596a05bc054d3f0dc1a
Juan Pablo Moreno <juanpablomoreno.r@gmail.com>
2023-12-11 02:25:09 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+/*
+CRUD ALUMNOS TKD
+
+1. Crear "Molde"(objeto) alumno
+    Tiene que tener los atributos minimos de un alumno, como su nombre, apellido, edad, clase
+2. Al iniciar el programa se debe de tener un array vacío donde guardaremos los alumnos que creemos
+3. Crear un menu donde el usuario pueda poner un input
+4. Con el uso de Switch, podremos acceder a cada una de las acciones que quiera realizar
+    Se pide:
+        > Ver alumnos
+        > Agregar Alumno
+        > Modificar Alumno
+        > Eliminar alumno
+        > Salir
+
+5. A futuro hacer que este CRUD no posea memoria volatil
+6. Diseño web
+
+*/
+
+// Array de objetos (global)
+
+
+
+// ¿Cual era aquel que tenía constructor y metodos integrados?
+// En el caso de poder ocupar aquello, crear el molde antes de la función
+
+// Función que muestra a los alumnos (loop del array)
+
+/*
+Función que crea alumnos
+    Crear objeto alumno
+    Serie de inputs, cada uno llenando un atributo
+    Agregar a array
+*/
+
+// Función que modifica el atributo del alumno
+    /*
+    Primero ha de preguntar cual atributo se quiere cambiár
+    Con el uso de Switch se cambia dicho atributo mediante un input
+     */
+
+// Función que elimina a un alumno
+    // Preguntar mediante input cual alumno ha de ser eliminado
+    // ¿Mostrar también a los alumnos registrados?
+    // Enocntrar al alumno en el array y eliminarlo de esta
+
+
+
+// Función que muestra el menú
